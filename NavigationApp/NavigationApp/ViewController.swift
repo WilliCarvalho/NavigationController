2aec9ac2a211b9e51aaec0b575d1aceffc099b06
Willian Carvalho <50246022+WilliCarvalho@users.noreply.github.com>
2022-08-16 09:48:34 -0300

    initial commit

---
+++ NavigationApp/NavigationApp/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  NavigationApp
+//
+//  Created by Usuário Convidado on 16/08/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func fechar (_sender: Any){
+        self.dismiss(animated: true)
+    }
+
+}
+
